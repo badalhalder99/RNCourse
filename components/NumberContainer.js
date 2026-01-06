c7e275386a01dfd6b8e0b5517736862eac3ebb3f
Badal Chandra Halder <badalchandrahalder99@gmail.com>
2026-01-06 10:48:31 +0600

    dimensions api working done

--- components/NumberContainer.js
+++ components/NumberContainer.js
@@ -1,22 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import Colors from '../css/color/Colors';
 
 function NumberContainer({ children }) {
-  return (
-    <View style={styles.container}>
+
+   return (
+      <View style={styles.container}>
       <Text style={styles.numberText}>{children}</Text>
-    </View>
-  );
+      </View>
+   );
 }
 
 export default NumberContainer;
+
+const screenWidth = Dimensions.get("window").width
 
 const styles = StyleSheet.create({
    container: {
       borderWidth: 4,
       borderColor: Colors.border ,
-      padding: 24,
-      margin: 24,
+      padding: screenWidth < 350 ? 16 : 24,
+      margin: screenWidth < 350 ? 16 : 24,
       borderRadius: 8,
       alignItems: 'center',
       justifyContent: 'center',
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,25 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import Colors from "../css/color/Colors";
 
 function GameOverScreen({ onRestart }) {
-  return (
-    <View style={styles.screen}>
-      <Text style={styles.text}>Game is over!</Text>
-      <PrimaryButton onPress={onRestart}>Restart Game</PrimaryButton>
-    </View>
-  );
+   return (
+      <View style={styles.screen}>
+         <Text style={styles.text}>Game is over!</Text>
+         <Image
+            style={styles.image}
+            source={require("../assets/icon.png")}
+            resizeMode="cover"
+         />
+         <PrimaryButton onPress={onRestart}>Restart Game</PrimaryButton>
+      </View>
+   );
 }
 
 export default GameOverScreen;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
+   screen: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   text: {
+      fontSize: 24,
+      marginBottom: 20,
+   },
+   image: {
+      width: deviceWidth < 350 ? 150 : 200,
+      height: deviceWidth < 350 ? 150 : 200,
+      marginBottom: 16,
+      borderWidth: 4,
+      borderRadius: deviceWidth < 350 ? 150 : 200,
+      borderColor:Colors.success,
+   }
 });
